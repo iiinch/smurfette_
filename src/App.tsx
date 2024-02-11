f6ed5979f22c8bdbe2fcb7563c25612d7158c03c
chaimaa <shimikawaii.123@gmail.com>
2024-02-11 14:27:03 +0100

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+function App () {
+return <Header />
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-
-
-
-function App() {
-  return (
-    <div>
-     <BrowserRouter>
-     <Routes>
-       <Route index element={<Home />}/>
-       <Route path="/home" element={<Home />}/>
-     </Routes>
-     </BrowserRouter>
-    </div>
-
-)
 }
-
-export default App;
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+return (
+ <nav className="nav">
+  <a href="/" className="site-title">Site Name</a>
+  <ul>
+    <li> 
+      <a href="/pricing">Pricing</a>
+      <a href="/about">about</a>
+    </li>
+  </ul>
+
+ </nav>
+
+)
+                                                                                                                                                                                                                                                                    
+}
