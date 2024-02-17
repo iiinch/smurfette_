174d8df1dbd8c832cd1ef29855be9e6a163c6f39
chaimaa <shimikawaii.123@gmail.com>
2024-02-17 19:21:39 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from "react"; 
+import React, { useEffect } from "react"; 
 
 import { supabase } from "./lib/helper/supabaseClient";
 
 export default function App() {
 
+ const login = async() => {
 
-  const login = async() => {
-    
-   await supabase.auth.signInWithOAuth({
+     await supabase.auth.signInWithOAuth({
+      provider:"github" 
 
-     provider:"github"
    });
-  }
-   return (
-       <div>
-       <button onClick={login}>Login with Github</button>
+  }; 
+    useEffect(() => {
       
-       </div>
+  const session = supabase.auth.sessions();
 
-  );
+console.log(session)
 
-}
+  },[])
+  return (
+   <div>
+  <button onClick={login}>Login with Github</button>
+  </div>
+);
+
+}   
